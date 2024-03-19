c4fe1cca6b28c0c2e7b54423ae85d7d35b1e7363
apurayy <apurayllpi@gmail.com>
2024-03-19 21:10:32 +0600

    2nd commit

--- auth/dashboard.php
+++ auth/dashboard.php
@@ -9,17 +9,12 @@ if (isset($_SESSION['username'])) {
     require_once("sidebar.php");
 
     require_once("topbar.php");
+
     require_once("main-content.php");
+    
     require_once("footer.php");
 
     ?>
-
-
-        
-
-       
-
-    <!--======================= Html_File_Include_End================ -->
 
     <?php } else {
     $msg = "You are not login!";
--- auth/sign-up.php
+++ auth/sign-up.php
@@ -1,31 +1,5 @@
+<?php require_once('header.php'); ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-  <title>E-Commerce Project</title>
-
-  <!-- GOOGLE FONTS -->
-  <link href="https://fonts.googleapis.com/css?family=Karla:400,700|Roboto" rel="stylesheet">
-  <link href="plugins/material/css/materialdesignicons.min.css" rel="stylesheet" />
-  <link href="plugins/simplebar/simplebar.css" rel="stylesheet" />
-
-  <!-- PLUGINS CSS STYLE -->
-  <link href="plugins/nprogress/nprogress.css" rel="stylesheet" />
-  
-  <!-- MONO CSS -->
-  <link id="main-css-href" rel="stylesheet" href="css/style.css" />
-
-  <!-- FAVICON -->
-  <link href="images/favicon.png" rel="shortcut icon" />
-  <script src="plugins/nprogress/nprogress.js"></script>
-</head>
-
-</head>
   <body class="bg-light-gray" id="body">
           <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh">
           <div class="d-flex flex-column justify-content-between">
